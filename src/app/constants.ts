d2d580df79da2fed59f311e128b1a7353a3a6db7
dathrika <sdaws1531@gmail.com>
2024-12-10 19:47:37 -0500

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a5a0029d2571e4f3495ab51500f19e55-1888408116.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a7de84a01bd6e468d8fe621a2d5047cc-2070338990.us-east-2.elb.amazonaws.com',
 };
